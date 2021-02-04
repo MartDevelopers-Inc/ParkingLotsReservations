7ea5e0500e8f5dfc715093081bb5580770acf4ae
MartMbithi <martdevelopers254@gmail.com>
2021-02-04 15:58:48 +0300

    Analytics On Dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,7 @@ require_once('../partials/head.php');
 <body>
 
     <!-- Navigation Bar-->
-    <?php require_once("../partials/admin_nav.php");?>
+    <?php require_once("../partials/admin_nav.php"); ?>
     <!-- End Navigation Bar-->
 
 
@@ -23,7 +23,7 @@ require_once('../partials/head.php');
                 <div class="col-sm-12">
                     <div class="page-title-box">
                         <div class="btn-group float-right m-t-15">
-                            
+
                         </div>
                         <h4 class="page-title">Dashboard</h4>
                     </div>
@@ -36,7 +36,7 @@ require_once('../partials/head.php');
                     <div class="card-box tilebox-one">
                         <i class="zmdi zmdi-traffic float-right text-muted"></i>
                         <h6 class="text-muted text-uppercase m-b-20">Parking Lots</h6>
-                        <h2 class="m-b-20" data-plugin="counterup">1,587</h2>
+                        <h2 class="m-b-20" data-plugin="counterup"><?php echo $parking_lots; ?></h2>
                     </div>
                 </div>
 
@@ -44,7 +44,7 @@ require_once('../partials/head.php');
                     <div class="card-box tilebox-one">
                         <i class="zmdi zmdi-money-box float-right text-muted"></i>
                         <h6 class="text-muted text-uppercase m-b-20">Reservations Revenue</h6>
-                        <h2 class="m-b-20">$<span data-plugin="counterup">46,782</span></h2>
+                        <h2 class="m-b-20">Ksh <span data-plugin="counterup"><?php echo $redervations_revenue; ?></span></h2>
                     </div>
                 </div>
 
@@ -52,7 +52,7 @@ require_once('../partials/head.php');
                     <div class="card-box tilebox-one">
                         <i class="zmdi zmdi-accounts-alt float-right text-muted"></i>
                         <h6 class="text-muted text-uppercase m-b-20">Clients</h6>
-                        <h2 class="m-b-20">$<span data-plugin="counterup">15.9</span></h2>
+                        <h2 class="m-b-20"><span data-plugin="counterup"><?php echo $clients; ?></span></h2>
                     </div>
                 </div>
             </div>
@@ -67,10 +67,12 @@ require_once('../partials/head.php');
                             <table class="table table-bordered mb-0">
                                 <thead>
                                     <tr>
-                                        <th>Company</th>
-                                        <th>Start Date</th>
-                                        <th>End Date</th>
-                                        <th>Status</th>
+                                        <th>Code</th>
+                                        <th>Client Name</th>
+                                        <th>Car RegNo</th>
+                                        <th>Parking Lot No</th>
+                                        <th>Parking Duration</th>
+                                        <th>Reservation Time</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -79,37 +81,10 @@ require_once('../partials/head.php');
                                         <td>20/02/2014</td>
                                         <td>19/02/2020</td>
                                         <td><span class="badge badge-success">Paid</span></td>
+                                        <td></td>
+                                        <td></td>
                                     </tr>
-                                    <tr>
-                                        <th class="text-muted">Envato Pty Ltd.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-danger">Unpaid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Dribbble LLC.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Adobe Family</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Apple Technology</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-danger">Unpaid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Envato Pty Ltd.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
+
                                 </tbody>
                             </table>
                         </div>
@@ -123,10 +98,11 @@ require_once('../partials/head.php');
                             <table class="table table-bordered mb-0">
                                 <thead>
                                     <tr>
-                                        <th>Company</th>
-                                        <th>Start Date</th>
-                                        <th>End Date</th>
-                                        <th>Status</th>
+                                        <th>Code</th>
+                                        <th>Amt Paid</th>
+                                        <th>Client Name</th>
+                                        <th>Client Phone No</th>
+                                        <th>Paid On</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -135,36 +111,7 @@ require_once('../partials/head.php');
                                         <td>20/02/2014</td>
                                         <td>19/02/2020</td>
                                         <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Envato Pty Ltd.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-danger">Unpaid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Dribbble LLC.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Adobe Family</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Apple Technology</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-danger">Unpaid</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th class="text-muted">Envato Pty Ltd.</th>
-                                        <td>20/02/2014</td>
-                                        <td>19/02/2020</td>
-                                        <td><span class="badge badge-success">Paid</span></td>
+                                        <th></th>
                                     </tr>
                                 </tbody>
                             </table>
@@ -174,130 +121,12 @@ require_once('../partials/head.php');
                 </div>
             </div>
 
-        </div> 
+        </div>
 
 
         <!-- Footer -->
         <?php require_once("../partials/footer.php"); ?>
         <!-- End Footer -->
-
-
-        <!-- Right Sidebar -->
-        <div class="side-bar right-bar">
-            <div class="nicescroll">
-                <ul class="nav nav-pills nav-justified">
-                    <li class="nav-item">
-                        <a href="#home-2" class="nav-link active" data-toggle="tab" aria-expanded="false">
-                            Activity
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#messages-2" class="nav-link" data-toggle="tab" aria-expanded="true">
-                            Settings
-                        </a>
-                    </li>
-                </ul>
-
-                <div class="tab-content">
-                    <div class="tab-pane fade active show" id="home-2">
-                        <div class="timeline-2">
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">5 minutes ago</small>
-                                    <p><strong><a href="#" class="text-info">John Doe</a></strong> Uploaded a photo <strong>"DSC000586.jpg"</strong></p>
-                                </div>
-                            </div>
-
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">30 minutes ago</small>
-                                    <p><a href="" class="text-info">Lorem</a> commented your post.</p>
-                                    <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
-                                </div>
-                            </div>
-
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">59 minutes ago</small>
-                                    <p><a href="" class="text-info">Jessi</a> attended a meeting with<a href="#" class="text-success">John Doe</a>.</p>
-                                    <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
-                                </div>
-                            </div>
-
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">1 hour ago</small>
-                                    <p><strong><a href="#" class="text-info">John Doe</a></strong>Uploaded 2 new photos</p>
-                                </div>
-                            </div>
-
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">3 hours ago</small>
-                                    <p><a href="" class="text-info">Lorem</a> commented your post.</p>
-                                    <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
-                                </div>
-                            </div>
-
-                            <div class="time-item">
-                                <div class="item-info">
-                                    <small class="text-muted">5 hours ago</small>
-                                    <p><a href="" class="text-info">Jessi</a> attended a meeting with<a href="#" class="text-success">John Doe</a>.</p>
-                                    <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="tab-pane fade" id="messages-2">
-
-                        <div class="row m-t-10">
-                            <div class="col-8">
-                                <h5 class="m-0">Notifications</h5>
-                                <p class="text-muted m-b-0"><small>Do you need them?</small></p>
-                            </div>
-                            <div class="col-4 text-right">
-                                <input type="checkbox" checked data-plugin="switchery" data-color="#1bb99a" data-size="small" />
-                            </div>
-                        </div>
-
-                        <div class="row m-t-10">
-                            <div class="col-8">
-                                <h5 class="m-0">API Access</h5>
-                                <p class="m-b-0 text-muted"><small>Enable/Disable access</small></p>
-                            </div>
-                            <div class="col-4 text-right">
-                                <input type="checkbox" checked data-plugin="switchery" data-color="#1bb99a" data-size="small" />
-                            </div>
-                        </div>
-
-                        <div class="row m-t-10">
-                            <div class="col-8">
-                                <h5 class="m-0">Auto Updates</h5>
-                                <p class="m-b-0 text-muted"><small>Keep up to date</small></p>
-                            </div>
-                            <div class="col-4 text-right">
-                                <input type="checkbox" checked data-plugin="switchery" data-color="#1bb99a" data-size="small" />
-                            </div>
-                        </div>
-
-                        <div class="row m-t-10">
-                            <div class="col-8">
-                                <h5 class="m-0">Online Status</h5>
-                                <p class="m-b-0 text-muted"><small>Show your status to all</small></p>
-                            </div>
-                            <div class="col-4 text-right">
-                                <input type="checkbox" checked data-plugin="switchery" data-color="#1bb99a" data-size="small" />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div> <!-- end nicescroll -->
-        </div>
-        <!-- /Right-bar -->
-
-
     </div> <!-- End wrapper -->
     <?php require_once("../partials/scripts.php"); ?>
 </body>
